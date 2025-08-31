beea68b9f26a71829afcebedd32fd59d9f5100b7
Phillycious78 <chillnrelaxmusic@gmail.com>
2025-08-31 14:44:00 +0200

    Added vite.config.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { DataProvider } from './contexts/DataContext.tsx'
+import { AuthProvider } from './contexts/AuthContext.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </DataProvider>
   </React.StrictMode>,
 )
